a8fa1a54ce77d020d692df8115469ea3bd397a65
kimocoder <christian@aircrack-ng.org>
2022-10-29 14:52:29 +0200

    add python3.11 to setup classifiers

--- setup.py
+++ setup.py
@@ -43,5 +43,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+
     ]
 )
