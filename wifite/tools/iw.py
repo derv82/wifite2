1c0056d58b16ef74a2bfeeeaf2641edc03491400
kimocoder <christian@aircrack-ng.org>
2022-11-05 14:40:00 +0100

    Fix overly permissive regular expression range

--- wifite/tools/iw.py
+++ wifite/tools/iw.py
@@ -24,7 +24,7 @@ class Iw(Dependency):
         import re
 
         ireg = re.compile(r"\s+Interface\s[a-zA-Z\d]+")
-        mreg = re.compile(r"\s+type\s[a-zA-z]+")
+        mreg = re.compile(r"\s+type\s[a-zA-z]\d+")
         ires = None
 
         interfaces = set()
