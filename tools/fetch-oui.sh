fdd41c3d6b91074b2af398ef486eea1f74a1f7ad
kimocoder <christian@aircrack-ng.org>
2022-11-09 18:19:28 +0100

    tools: Update fetch-oui script

--- tools/fetch-oui.sh
+++ tools/fetch-oui.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+#
+# Fetches the latest additions to the OUI db (for manufacturers)
+# which we parse by utilizing the "--showm" parameter in Wifite.
+#
+# more manufacturers in the list is more knowledge.
+#
+
 echo "* Fetching a fresh OUI (manufacturers) list  ..."
-get-oui -f new-ieee-oui.txt -u http://standards-oui.ieee.org/oui/oui.txt
+get-oui -f ieee-oui.txt -u http://standards-oui.ieee.org/oui/oui.txt
