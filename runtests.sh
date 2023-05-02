73426ada099583ce0d27d128086605f02393968a
kimocoder <christian@aircrack-ng.org>
2023-05-02 21:24:41 +0200

    Update runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,13 +1,3 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-#echo "** Starting flake8 tests .."
-#sleep 2
-#sh tests/test_flake8.sh
-#sleep 2
-clear
-
-echo "** Starting unittest/pytest .."
-sleep 2
-python3 -m unittest discover tests -v
-sleep 2
-echo "** runtests.sh test script at the end"
+pytest -s tests/ -v
