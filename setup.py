27668d31c89a08561b8ef9fb3b3dcbc74059b2fd
kimocoder <christian@aircrack-ng.org>
2023-05-02 20:16:17 +0200

    add python-3.12 classifier

--- setup.py
+++ setup.py
@@ -47,5 +47,6 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ]
 )
