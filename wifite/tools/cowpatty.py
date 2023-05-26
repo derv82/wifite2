2ce08b66723e912928b03af7dfed832a89b5d3ca
kimocoder <christian@aircrack-ng.org>
2023-05-26 15:02:02 +0200

    coWPatty is now fixed and working

--- wifite/tools/cowpatty.py
+++ wifite/tools/cowpatty.py
@@ -5,29 +5,37 @@ from .dependency import Dependency
 from ..config import Configuration
 from ..util.color import Color
 from ..util.process import Process
+from ..tools.hashcat import HcxPcapngTool
+
+import os
+import re
 
 
 class Cowpatty(Dependency):
-    """ Wrapper for Cowpatty program. """
+    ''' Wrapper for Cowpatty program. '''
     dependency_required = False
     dependency_name = 'cowpatty'
     dependency_url = 'https://tools.kali.org/wireless-attacks/cowpatty'
+
 
     @staticmethod
     def crack_handshake(handshake, show_command=False):
         # Crack john file
         command = [
             'cowpatty',
-             '-f',
-             Configuration.wordlist,
-             '-r',
-             handshake.capfile,
-             '-s',
-             f'"{handshake.essid}"',
+            '-f', Configuration.wordlist,
+            '-r', handshake.capfile,
+            '-s', handshake.essid
         ]
         if show_command:
             Color.pl('{+} {D}Running: {W}{P}%s{W}' % ' '.join(command))
         process = Process(command)
         stdout, stderr = process.get_output()
 
-        return next((line.split('"', 1)[1][:-2] for line in stdout.split('\n') if 'The PSK is "' in line), None)
+        key = None
+        for line in stdout.split('\n'):
+            if 'The PSK is "' in line:
+                key = line.split('"', 1)[1][:-2]
+                break
+
+        return key
